a6ed656d9dae25dee9276af5064ca15b3c2df94c
Salymka <prokopchuk.dmytro01@gmail.com>
2023-05-06 18:17:53 +0300

    init

---
+++ src/components/App/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from './App.module.scss'
+function App() {
+  return (
+    <div className={styles.app}>
+      Work
+    </div>
+  );
+}
+
+export default App;
